86eebb88449c4eed3ad31cd15db331f1c5e29527
Pitchaya Namchaisiri <pea14733@gmail.com>
2017-02-25 02:58:52 +0700

    Initialized Git. Completed basic pathfinding. Starting turret lockon

---
+++ Assets/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	float speed = 15f;
+	GameObject path;
+	Transform targetPathNode;
+	int pathNodeIndex = 0;
+	// Use this for initialization
+	void Start () {
+		path = GameObject.Find("Path");
+	}
+	void GetNextPathNode() {
+		targetPathNode = path.transform.GetChild(pathNodeIndex);
+		pathNodeIndex++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(targetPathNode == null) {
+			GetNextPathNode();
+			if(targetPathNode == null) {
+				ReachedFinalNode();
+			}
+		}
+		Vector3 dir = targetPathNode.position - this.transform.localPosition;
+		float distThisFrame = speed * Time.deltaTime;
+		if(dir.magnitude <= distThisFrame) {
+			targetPathNode = null;
+		}
+		else {
+			transform.Translate(dir.normalized * distThisFrame, Space.World);
+			Quaternion targetRotation = Quaternion.LookRotation(dir);
+			this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, distThisFrame);
+		}
+
+	}
+	void ReachedFinalNode() {
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
+
+		Enemy nearestEnemy = null;
+		float dist = Mathf.Infinity;
+	}
+}
