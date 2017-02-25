3b1a8e798689f0f0879355b60e69e3e6b82bdf3a
Pitchaya Namchaisiri <pea14733@gmail.com>
2017-02-25 12:03:27 +0700

    Completed turrent lockon. Completed bullet shooting. Starting damage.

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -2,42 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-	float speed = 15f;
-	public float health = 10f;
-	GameObject path;
-	Transform targetPathNode;
-	int pathNodeIndex = 0;
+public class Bullet : MonoBehaviour {
+	public float speed = 60f;
+ 	public float damage = 1f;
+
+	public Transform target;
 	// Use this for initialization
 	void Start () {
-		path = GameObject.Find("Path");
-	}
-	void GetNextPathNode() {
-		targetPathNode = path.transform.GetChild(pathNodeIndex);
-		pathNodeIndex++;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(targetPathNode == null) {
-			GetNextPathNode();
-			if(targetPathNode == null) {
-				ReachedFinalNode();
-			}
-		}
-		Vector3 dir = targetPathNode.position - this.transform.localPosition;
+		Vector3 dir = target.position - this.transform.localPosition;
 		float distThisFrame = speed * Time.deltaTime;
 		if(dir.magnitude <= distThisFrame) {
-			targetPathNode = null;
+			BulletHit();
 		}
 		else {
 			transform.Translate(dir.normalized * distThisFrame, Space.World);
 			Quaternion targetRotation = Quaternion.LookRotation(dir);
 			this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, distThisFrame);
+			
 		}
 
+
 	}
-	void ReachedFinalNode() {
+	void BulletHit() {
+		gameObject.GetComponent<Enemy>().health -= 1;
 		Destroy(this.gameObject);
 	}
 }
--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -3,17 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour {
-
+	Transform turretTransform;
+	float range = 25f;
+	public float damage = 1f;
+	public GameObject bulletPref;
 	// Use this for initialization
+	float attackSpeed = 0.2f;
+	float attackSpeedLeft = 0;
 	void Start () {
 		
+		turretTransform = transform.Find("Turret");
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 		Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
 
 		Enemy nearestEnemy = null;
 		float dist = Mathf.Infinity;
+
+		foreach(Enemy e in enemies) {
+			float d = Vector3.Distance(this.transform.position, e.transform.position);
+			if(nearestEnemy == null || d < dist) {
+				nearestEnemy = e;
+				dist = d;
+			}
+		}
+
+		if(nearestEnemy == null) {
+			Debug.Log("No enemies detected");
+			return;
+		}
+		Vector3 dir = nearestEnemy.transform.position - this.transform.position;
+		Quaternion lookRot = Quaternion.LookRotation(dir);
+		turretTransform.rotation = Quaternion.Euler( turretTransform.eulerAngles.x, lookRot.eulerAngles.y, turretTransform.eulerAngles.z);
+		
+		attackSpeedLeft -= Time.deltaTime;
+		if(attackSpeedLeft <= 0 && dir.magnitude <= range) {
+			attackSpeedLeft = attackSpeed;
+			AttackOn(nearestEnemy);
+		}
+	}
+
+	void AttackOn(Enemy e) {
+		GameObject bulletObj = (GameObject)Instantiate(bulletPref,this.transform.position, this.transform.rotation);
+		Bullet b = bulletObj.GetComponent<Bullet>();
+		b.target = e.transform;
+
+
+
 	}
 }
